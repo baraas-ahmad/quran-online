d3e97d8a65a9edcd960529bdb1c62d61310b7b4a
abi hosein <abidevmail@gmail.com>
2025-04-20 22:52:40 +0800

    add style

--- app.js
+++ app.js
@@ -4,31 +4,42 @@ async function getSurahList() {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        console.log('Surah List:', data);
         return data.data;
 
-        // console.log('Surah List:', data.data);
+       
+
     } catch (error) {
         console.error('Error fetching surah list:', error);
     }
 }
 
 //template UI Surah
-function setSurahTemplate(number, name, translation) {
-    return `${number} - ${name} (${translation})`;
+function setSurahTemplate(number, arabicName, englishName, translation) {
+    return `
+    <div class="py-4 px-4 pb-4 pt-4 border-b border-gray-400 hover:bg-sky-100">
+       <a href="pages/surah.html?number=${number}" class="text-lg">
+        <div class="flex gap-4">
+            <span class="">${number}.</span> ${arabicName} 
+        </div>
+        <div class="flex gap-4 text-sm text-gray-500">
+            <span>&nbsp;</span>${englishName} (${translation})
+        </div>
+    </a>
+    </div>`;
 }
+
 
 
 //get element list surah
 async function getElementSurahList() {
     try {
         const surahList = await getSurahList();
-        console.log('Surah List:', surahList);
-
-        let surahElements = '';
-        for (let i = 0; i < surahList.length; i++) {
-            surahElements += setSurahTemplate(surahList[i].number, surahList[i].name, surahList[i].englishName) + '<br>';
-        }
-        return surahElements;
+        
+        let surahElements = surahList.map((surah) => {
+            return setSurahTemplate(surah.number, surah.name, surah.englishName, surah.englishNameTranslation);
+        })
+        return surahElements.join('');a
     } catch (error) {
         return 'Error fetching surah list';
     }
@@ -40,7 +51,6 @@ async function renderAll() {
     const renderedSurahList = await getElementSurahList();
     app.innerHTML = renderedSurahList;
 }
-
 
 
 renderAll();
